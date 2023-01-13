e51bb9116a13cdb80a7d21a53717932bd2c8b373
idriss667 <idrisgouja@yahoo.com>
2023-01-13 10:31:38 +0100

    add back

--- php/app/src/Controller/FlatsharingController.php
+++ php/app/src/Controller/FlatsharingController.php
@@ -15,24 +15,24 @@ class FlatsharingController extends AbstractController
     #[Route('/flatsharing', name: "flatsharing", methods: ["GET"])]
     public function home()
     {   
-        $tok = getallheaders()['authorization'];
-        $secretKey  = 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=';
-        $token = JWT::decode($tok, new Key($secretKey, 'HS512'));
-        $now = new DateTimeImmutable();
-        $serverName = "localhost";
+        // $tok = getallheaders()['authorization'];
+        // $secretKey  = 'bGS6lzFqvvSQ8ALbOxatm7/Vk7mLQyzqaS34Q4oR1ew=';
+        // $token = JWT::decode($tok, new Key($secretKey, 'HS512'));
+        // $now = new DateTimeImmutable();
+        // $serverName = "localhost";
 
-        if ($token->iss !== $serverName ||
-            $token->nbf > $now->getTimestamp() ||
-            $token->exp < $now->getTimestamp())
-        {
-            echo 'Token not found3';
-            exit;
-        }
-        
+        // if ($token->iss !== $serverName ||
+        //     $token->nbf > $now->getTimestamp() ||
+        //     $token->exp < $now->getTimestamp())
+        // {
+        //     echo 'Token not found3';
+        //     exit;
+        // }
         $manager = new FlatsharingManager(new PDOFactory());
-        $id = $token->ID;
+        // $id = $token->ID;
+        $id = 1;
         var_dump($id);
-        $flatsharing = $manager->getUserFlatsharing($id);
+        $flatsharing = $manager->CreateFlatsharing($id, 'test');
         echo json_encode($flatsharing);
         exit;
     }
